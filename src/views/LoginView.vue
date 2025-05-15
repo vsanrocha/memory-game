9ab9564c72ee7acce8dac28465219f07002f6ce6
Vinicius Rocha <vsanrocha@gmail.com>
2025-05-15 10:38:45 -0300

    feat: add LoginView component with Google login integration

---
+++ src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const callback = (response) => {
+  // This callback will be triggered when the user selects or login to
+  // his Google account from the popup
+  console.log('Handle the response', response)
+  router.push('/home')
+}
+</script>
+<template>
+  <div class="min-h-screen flex justify-center items-center">
+    <GoogleLogin :callback="callback" />
+  </div>
+</template>
